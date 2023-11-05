4cac52a4c7682613bd3c3fac0d05e78ac3d4e8e3
topanim <gorogannisan641@gmail.com>
2023-11-05 22:11:22 +0300

    commit

--- frontend/scripts/upload_data.js
+++ frontend/scripts/upload_data.js
@@ -1,10 +1,22 @@
-const main = document.getElementsByTagName('main')[0];
+const main = document.querySelector('.content');
 console.log(main);
 const data_path = "https://api.jsonbin.io/v3/b/65469f0412a5d3765994e4a0"
 fetch(data_path)
   .then(file => file.json())
   .then(data => data.record.forEach((hero) => {
 	console.log(`background: linear-gradient(45deg, ${hero.bottom_color} ${hero.bottom}, ${hero.top_color} ${hero.top});`);
-	let item = `<div class="item" style="background: linear-gradient(45deg, ${hero.bottom_color} ${hero.bottom}, ${hero.top_color} ${hero.top});"><div class="hero"><div class="hero-icon-block"><img class="hero-icon" src="${hero.icon}"></div><div class="about"><p class="hero-name">${hero.name}</p><p class="hero-about">${hero.description}</p></div><a class="learn-more" href="${hero.about}" target="_blank">LEARN MORE</a></div></div>`;
+	let item = `
+    <div class="item" style="background: linear-gradient(45deg, ${hero.bottom_color} ${hero.bottom}, ${hero.top_color} ${hero.top});">
+      <div class="hero">
+        <div class="hero-icon-block">
+          <img class="hero-icon" src="${hero.icon}">
+        </div>
+        <div class="about">
+          <p class="hero-name">${hero.name}</p>
+          <p class="hero-about"><strong>Anime:</strong> ${hero.anime}</p>
+        </div>
+        <a class="learn-more" href="${hero.about}" target="_blank">LEARN MORE</a>
+      </div>
+    </div>`;
   	main.insertAdjacentHTML("beforeEnd", item);
   }));
